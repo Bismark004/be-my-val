a10746f32f604bc9606c17dfd1f9dc2ebe381979
Bismark Adzai <adzaibismark@gmail.com>
2024-02-04 20:57:57 +0000

    commit

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const noButton = document.getElementById('no');
 
 noButton.addEventListener('mousemove', function(event) {
-  let x = Math.random() * window.innerWidth;
-  let y = Math.random() * window.innerHeight;
+ const mouseX = event.clientX;
+ const mouseY = event.clientY;
 
-  noButton.style.left = x + "px";
-  noButton.style.top = y + "px";
+
+ const buttonRect = noButton.getBoundingClientRect();
+ const buttonX = buttonRect.left + buttonRect.width / 2;
+ const buttonY = buttonRect.top + buttonRect.height /2;
+
+ const distance = Math.sqrt((mouseX - buttonX) ** 2 + (mouseY - buttonY) ** 2);
+
+ if (distance < 50) {
+    moveButtonRandomly();
+ }
 });
+
+noButton.addEventListener('click', moveButtonRandomly);
+
+function moveButtonRandomly() {
+    let x = Math.random() * (window.innerWidth - noButton.clientWidth);
+    let y = Math.random() * (window.innerHeight - noButton.clientHeight);
+
+    noButton.style.left = x + "px";
+    noButton.style.top = y + "px";
+}
