9d72a926e0a617b3893cc10426af3387d662dd5b
Bismark Adzai <adzaibismark@gmail.com>
2024-02-04 20:35:35 +0000

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const noButton = document.getElementById('no');
 
-noButton.addEventListener('mousemove', function() {
+noButton.addEventListener('mousemove', function(event) {
   let x = Math.random() * window.innerWidth;
   let y = Math.random() * window.innerHeight;
 
