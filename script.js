6c3b6f93ed8b10dd5320c316f08356d7b6ee5662
Bismark Adzai <adzaibismark@gmail.com>
2024-02-04 20:34:52 +0000

    commit

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-let yesButton = document.getElementById('yes');
+const noButton = document.getElementById('no');
 
-let noButton = document.getElementById('no');
+noButton.addEventListener('mousemove', function() {
+  let x = Math.random() * window.innerWidth;
+  let y = Math.random() * window.innerHeight;
 
-yesButton.addEventListener('mousemove', function() {
-
-});
-
-noButton.addEventListener('mousemove', function(event) {
-    const buttonRect = noButton.getBoundingClientRect();
-    const distanceX = event.clientX - (buttonRect.left + buttonRect.width / 2);
-
-    noButton.style.transform = `translateX(${Math.min(Math.max(distanceX, -50), 50)}px)`;
-});
-
-noButton.addEventListener('mouseleave', function() {
-    noButton.style.transform = 'translateX(0)';
+  noButton.style.left = x + "px";
+  noButton.style.top = y + "px";
 });
