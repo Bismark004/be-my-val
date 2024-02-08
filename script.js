02d57f24af5c03380c957cf2abf6bef08d819ee4
Bismark Adzai <adzaibismark@gmail.com>
2024-02-08 00:07:24 +0000

    commit

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-const button = document.getElementById('no');
-const threshold = 100;
+const noButton = document.getElementById('no');
 
-window.addEventListener('mousemove', function(e){
-    let x = e.clientX;
-    let y = e.clientY;
-
-    let rect = button.getBoundingClientRect();
-    let bx = rect.left + rect.width / 2;
-    let by = rect.top + rect.height / 2;
-
-    let dx = x - bx;
-    let dy = y - by;
-    let distance = Math.sqrt(dx * dx + dy * dy);
-
-    if (distance < threshold) {
-        let rx = Math.random() * (window.innerWidth - rect.width);
-        let ry = Math.random() * (window.innerHeight - rect.height);
-
-        button.style.position = 'absolute';
-        button.style.left = rx + "px";
-        button.style.top = ry + "px";
-    }
+noButton.addEventListener('click', function(e) {
+    alert('Nice try');
 });
+
