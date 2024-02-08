7ef3cc2ceb6517724431701cb5c3baea1b13f54d
Bismark Adzai <adzaibismark@gmail.com>
2024-02-08 00:17:33 +0000

    commit

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ const noButton = document.getElementById('no');
 
 noButton.addEventListener('click', function(e) {
     alert('Nice try');
+    window.close();
+});
+
+document.addEventListener('mousemove', function(e) {
+    const x = e.pageX;
+    const Y = e.pageY;
+    const buttonBox = noButton.getBoundingClientRect();
+    const horizontalDistanceFrom = distanceFromCenter(buttonBox.x, x, buttonBox.width);
+    const verticalDistanceFrom = distanceFromCenter(buttonBox.y, y, buttonBox.height);
 });
 
